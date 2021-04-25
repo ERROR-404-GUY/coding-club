c4cf37f2cf0a4f88f01d37f44aa9965fb888d587
Huy Dinh <you@example.com>
2021-04-25 20:59:36 +0800

    Add static adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,14 @@
+const static = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+	kit: {
+		adapter: static({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	}
 };
