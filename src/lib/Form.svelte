8ec3cff099dd06ef37181ce24bccaa7d0f4a13f9
ERROR 404 GUY <weibeau@Huys-MacBook-Air.local>
2021-04-30 09:30:19 +0800

    change level

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -48,12 +48,12 @@
 			your level:
 			<select name="level" required>
 				<option value="">(Please be honest)</option>
-				<option value="0">0</option>
-				<option value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-				<option value="4">4</option>
-				<option value="5">5</option>
+				<option value=0>0</option>
+				<option value=1>1</option>
+				<option value=2>2</option>
+				<option value=3>3</option>
+				<option value=4>4</option>
+				<option value=5>5</option>
 			</select>
 		</label>
 
